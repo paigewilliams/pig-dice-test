f3fe2721fcd6eb85c923303a9003c332abdd423a
Paige Williams and Tanvi Garg <paige@>
2018-10-31 13:23:26 -0700

    Create Score constructor and create function to add dice rolls to each other.

--- js/scripts.js
+++ js/scripts.js
@@ -9,25 +9,33 @@ Game.prototype.addPlayer = function(player){
 //Business Logic for Players
 function Player (player, score){
   this.playerName = player;
-  this.score = score;
+  this.score =  new Score(0);
 }
 
-Player.prototype.diceRoll = function(min, max) {
+//Business logic for score
+function Score (scoreValue){
+  this.scoreValue = scoreValue;
+}
+
+Score.prototype.diceRoll = function(min, max) {
     min = 1;
     max = 7;
-    return Math.floor(Math.random() * (max-min));
-    // console.log(this.diceRoll());
+    return Math.floor(Math.random() * (max-min) + min);
 }
 
-Player.prototype.gamePlay = function(rollResult) {
-    this.score = this.diceRoll();
-    console.log(this.score);
+Score.prototype.gamePlay = function(rollResult) {
+    //this.score = this.diceRoll();
+    // rollResult = this.diceRoll();
+    console.log(rollResult);
+    this.scoreValue += rollResult;
+    console.log(this.scoreValue);
 }
-
 // Player.prototype.addScore = function(score) {
-//     this.score +=
+//     this.dice();
 // }
 // User Interface Logic
 var game = new Game
 var player1 = new Player("Paige", 2)
 game.addPlayer(player1)
+player1.score.gamePlay(player1.score.diceRoll())
+// player1.gamePlay(diceRoll());
