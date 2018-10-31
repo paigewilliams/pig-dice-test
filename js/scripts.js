12f8c11c6df89ecf39019390ca2af956c28c7484
Paige Williams and Tanvi Garg <tanvi@>
2018-10-31 11:05:42 -0700

    Create constructor for Game and Player.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,27 @@
 //Business Logic for Game
 function Game (){
-  this.player = []
-}
-//Business Logic for Players
-function Player (score){
-  this.score = 0;
+  this.players = [];
 }
 
+Game.prototype.addPlayer = function(player){
+  this.players.push(player);
+}
+//Business Logic for Players
+function Player (player, score){
+  this.playerName = player;
+  this.score = score;
+}
+
+// Player.prototype.diceRoll() = function(min, max) {
+//     min = 1;
+//     max = 7;
+//     score = Math.floor(Math.random() * (max-min)) + min;
+//     return this.score
+//
+// }
+
+
 // User Interface Logic
+var game = new Game
+var player1 = new Player("Paige", 2)
+game.addPlayer(player1)
